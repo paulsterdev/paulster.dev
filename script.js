a429a11c8144910c72e29eae11d05af5a77cd310
paulster <commits@paulster.dev>
2026-05-30 18:21:32 -0500

    Organized main page, added code for projectdatabase parsing

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ fetch("header.html")
     .then(data => {
         document.getElementById("header-menu").innerHTML = data;
     });
+
+
+fetch("projects.json")
+    .then(response => response.json())
+    .then(projects => {
+        const project_container = document.getElementById("project-card-container");
+        projects.forEach(project =>{
+            const projectCard = document.createElement("div");
+            projectCard.classList.add("project-details");
+            projectCard.innerHTML =  
+                `
+                <h1 class=heading4> ${project.title} </h1>
+            
+                `
+            const separator = document.createElement("div");
+            separator.classList.add("separator");
+            project_container.appendChild(projectCard);
+            project_container.appendChild(separator);
+            });
+        });
